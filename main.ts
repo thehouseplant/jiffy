b1c6fc484ef2211ec0dedb046587f79d314d1ebd
Sean Collins <sean.collins@outlook.com>
2023-07-23 13:21:24 -0700

    Add metadata helpers for troubleshooting

--- main.ts
+++ main.ts
@@ -5,6 +5,9 @@ await new Command()
   .name("jiffy")
   .version("0.1.0")
   .description("An opinionated CLI tool for interacting with the Jira API")
+  .meta("deno", Deno.version.deno)
+  .meta("v8", Deno.version.v8)
+  .meta("typescript", Deno.version.typescript)
   .parse(Deno.args);
 
 // Initialization prompt
