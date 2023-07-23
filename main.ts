47f7d8da36ccbdfc2eb554eda9a9bf1a099ab999
Sean Collins <sean.collins@outlook.com>
2023-07-23 11:23:59 -0700

    Add basic prompt command

--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { Confirm, Input, Secret, Select } from "https://deno.land/x/cliffy@v1.0.0-rc.2/prompt/mod.ts";
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+let apikey: string, email: string, team: string;
+
+await main();
+
+async function main() {
+  email = await Input.prompt({
+    message: "Enter your Tyler email address",
+  });
+
+  team = await Select.prompt({
+    message: "Select your team",
+    options: [
+      "Access Control",
+      "Eagle",
+      "Elumitas",
+      "Ingress & Queries",
+      "Interactive Reporting",
+      "Release",
+      "Security",
+      "TIRE",
+    ],
+  });
+
+  apikey = await Secret.prompt({
+    message: "Enter your Atlassian API key",
+  });
+
+  console.log({ email, team, apikey });
+  if (!await Confirm.prompt("Is everything correct?")) {
+    await main();
+  }
 }
