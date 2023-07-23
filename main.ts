70d9639ca93a5edbef106fd83d34f5d36053ecfe
Sean Collins <sean.collins@outlook.com>
2023-07-23 13:20:41 -0700

    Add first Jiffy command with version and description

--- main.ts
+++ main.ts
@@ -1,5 +1,13 @@
+import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.2/command/mod.ts";
 import { Input, Secret, Select, prompt } from "https://deno.land/x/cliffy@v1.0.0-rc.2/prompt/mod.ts";
 
+await new Command()
+  .name("jiffy")
+  .version("0.1.0")
+  .description("An opinionated CLI tool for interacting with the Jira API")
+  .parse(Deno.args);
+
+// Initialization prompt
 const result = await prompt([
   {
     name: "email",
